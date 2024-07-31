c464595b1f16296ad007a532bc35f78582ef364b
Victor White <victorblanco281201@gmail.com>
2024-07-30 22:29:42 -0400

    Done until 90 video

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+    <input type="text" placeholder="Search GIFs..." class="form-control"/>
+  `,
+})
+export class SearchBoxComponent {
+
+}
